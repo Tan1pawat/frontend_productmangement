b8b79cc5c73dc0ba86eb3eeaab2fd15b7e9a40bd
Pawat Yamwong <tancoding2023@gmail.com>
2025-03-25 14:09:13 +0700

    feat: add PrisonForm and PrisonFormEdit components with basic templates and styles

---
+++ src/app/components/prison/prison-form-edit/prison-form-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prison-form-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './prison-form-edit.component.html',
+  styleUrl: './prison-form-edit.component.scss'
+})
+export class PrisonFormEditComponent {
+
+}
---
+++ src/app/components/prison/prison-form/prison-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prison-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './prison-form.component.html',
+  styleUrl: './prison-form.component.scss'
+})
+export class PrisonFormComponent {
+
+}
