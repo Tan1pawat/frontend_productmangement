732b1de38923be19d5c674256d5e8053f7ff049b
Pawat Yamwong <tancoding2023@gmail.com>
2025-03-25 10:50:14 +0700

    feat: add Company and Prison components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductComponent } from './components/product/product.component';
 import { BillComponent } from './components/bill/bill.component';
+import { CompanyComponent } from './components/company/company.component';
+import { PrisonComponent } from './components/prison/prison.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboards', pathMatch: 'full' },
     { path: 'dashboards', component: DashboardComponent },
     { path: 'products', component: ProductComponent },
-    { path: 'bills', component: BillComponent }
+    { path: 'bills', component: BillComponent },
+    { path: 'company', component: CompanyComponent },
+    { path: 'prison',component:PrisonComponent}
 ];
---
+++ src/app/components/prison/prison.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prison',
+  standalone: true,
+  imports: [],
+  templateUrl: './prison.component.html',
+  styleUrl: './prison.component.scss'
+})
+export class PrisonComponent {
+
+}
